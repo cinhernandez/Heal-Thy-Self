dedaa25fb73c5a26577e5b6ed5754df2670c7503
Cindy Hernandez <cindyh@mail.com>
2023-05-16 16:47:50 -0700

    fix

--- lib/db/models.py
+++ lib/db/models.py
@@ -9,7 +9,7 @@ Base = declarative_base()
 class SuperHero(Base):
     __tablename__ = 'super_heroes'
 
-    __table_args__ =(PrimaryKeyConstraint('id'))
+    __table_args__ = (PrimaryKeyConstraint('id'))
     id = Column(Integer()) 
-    name - Column(String())
+    name = Column(String())
     power = Column(String())
