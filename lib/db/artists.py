f6c2dd6f8295ccef0ead487a31dfa3cbca839f36
Cindy Hernandez <cindyh@mail.com>
2023-05-16 19:57:12 -0700

    initial commit

--- lib/db/artists.py
+++ lib/db/artists.py
@@ -1,8 +1,10 @@
 
 from sqlalchemy import (PrimaryKeyConstraint, Column, String, Integer)
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-
+engine = create_engine('sqlite:///db.sqlite3')
 Base = declarative_base()
 
 
--- lib/db/genre.py
+++ lib/db/genre.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy import (PrimaryKeyConstraint, Column, String, Integer, Date)
+from sqlalchemy import (PrimaryKeyConstraint, Column, String, Integer)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,14 +8,13 @@ engine = create_engine('sqlite:///db.sqlite3')
 Base = declarative_base()
 
 
-class Event(Base):
-    __tablename__ = "events"
+class Genre(Base):
+    __tablename__ = "genres"
 
     __table_args__ = (PrimaryKeyConstraint('id'), )
    
 
     id = Column(Integer())
     name = Column(String())
-    location = Column(String())
-    date = Column(Date())
+    genre = Column(String())
     
