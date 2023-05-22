2eb521b85d4cf332eb6b46de19c17d61e1dd2da1
Cindy Hernandez <cindyh@mail.com>
2023-05-22 09:49:54 -0700

    updated

--- lib/cli.py
+++ lib/cli.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-from db.models import (Base, Artist, Festival)
+from db.models import (Base, Genre_Option, Festival, Add_To_Cart)
 
 
 engine = create_engine('sqlite:///edm.db')
@@ -44,71 +44,9 @@ if __name__ == '__main__':
     user_location = input(">>> ")
     clear()
 
-    print("Choose an option")
-    print("111\t=>\tSee all EDM Festivals in your area")
-    print("222\t=>\tSee all Artists")
-    print("333\t=>\tSee all Accommodations")
-    print("444\t=>\tSee all Festivals")
-    print("555\t=>\tSee all Festivals with Accommodations")
-    print("666\t=>\tSee all Festivals with Artists")
-    print("777\t=>\tSee all Festivals with Artists and Accommodations")
-    print("888\t=>\tSee all Festivals with Artists and Accommodations in your area")
-    print("999\t=>\tSee all Festivals with Artists and Accommodations in your area and their prices")
+    print("Here is a list of all the genres:")
+    genre_options = session.query(Genre_Option)
+    create_genre_options_table(genre_options)
 
-    user_input = input(">>> ")
-
-    if user_input == "111":
-        #clear()
-        print("Here are all the Genres:")
-        for genre_optiions in session.query(Genre).all():
-            print(genre_optiions.name)
-
-    elif user_input == "222":
-        clear()
-        print("Here are all the Artists:")
-        for artists in session.query(Artist).all():
-            print(artists.name)
-        print("Press any key to exit")
-        input(">>> ")
-        clear()
     
-    # elif user_input == "333":
-    #     clear()
-    #     print("Here are all the Accommodations:")
-    #     for accommadation in session.query(Accommadation).all():
-    #         print(accommadation.name)
-    #     print("Press any key to exit")
-    #     input(">>> ")
-    #     clear()
-
-    elif user_input == "444":
-        clear()
-        print("Here are all the Festivals:")
-        for festival in session.query(Festival).all():  
-            print(festival.name)    
-        print("Press any key to exit")
-        input(">>> ")
-        clear()
-
-    elif user_input == "555":
-        clear()
-        print("Here are all the Festivals with Accommodations:")
-        for festival in session.query(Festival).all():
-            if festival.accommadation:
-                print(festival.name)
-        print("Press any key to exit")
-        input(">>> ")
-        clear()
-
-    else:
-        print("Invalid input")
-        print("Press any key to exit")
-        input(">>> ")
-        clear()
-
-    print("Thank you for using the EDM Festival Finder!")
-    print("Goodbye!")
-    print("Press any key to exit")
-    input(">>> ")
-    clear()
-
+   
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,2 +1,4 @@
-from db.models import (Base, Artist, Accommadation, Festival, AddToCart, Genre_Options)
+from db.models import (Base, Festival, Add_To_Cart, Genre_Option)
 
+def create_genere_table(genre_options):
+    
